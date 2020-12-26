0730023a7181dd2184ff1e7259cebe66d25af255
ivan.silva <ivan.elisandro@gmail.com>
2020-12-26 17:17:17 -0300

    CHANGED: Implementation of Stage 2 of the Hypercar Service Center project. Created during my studies using JetBrains Academy.
    This stage creates a menu structure for the website.
    
    Important files:
    HyperNews Portal/task/hypercar/hypercar/urls.py
    HyperNews Portal/task/hypercar/tickets/templates/tickets/menu.html
    HyperNews Portal/task/hypercar/tickets/views.py

--- Hypercar Service Center/task/hypercar/tickets/views.py
+++ Hypercar Service Center/task/hypercar/tickets/views.py
@@ -1,7 +1,22 @@
 from django.views import View
+from django.views.generic.base import TemplateView
 from django.http.response import HttpResponse
+from django.shortcuts import render
+
+menu = [
+    {"name": "Change oil", "path": "change_oil"},
+    {"name": "Inflate tires", "path": "inflate_tires"},
+    {"name": "Get diagnostic test", "path": "diagnostic"},
+]
 
 
 class WelcomeView(View):
     def get(self, request, *args, **kwargs):
         return HttpResponse('<h2>Welcome to the Hypercar Service!</h2>')
+
+
+class MenuView(TemplateView):
+    template_name = 'tickets/menu.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'menu': menu})
